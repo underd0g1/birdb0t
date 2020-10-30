e4809b545c76e1df8b4b8e7633d1e897bca4cc8d
underd0g1 <hide4@comcast.net>
2020-10-29 21:10:47 -0400

    added mvc base

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  consumer_key:         'iPKcerCW1JmKUfQ880ERn8POP',
+  consumer_secret:      'TaPHpZS6bhD1P5VEj8p5lVZ95GiLOccwySDbXRDzgrfcbOT88P',
+  access_token:         '1212777559819079680-4nYVYbkIj8t6RUwwmQFIUGO4210ujQ',
+  access_token_secret:  'JtIVh8XD7a8P4FqK3oh35K1bjPgjMb5np2ZVsnfdWAc1j',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            false,     // optional - requires SSL certificates to be valid
+
+};
